c72681a0bca6b9cc0e896b1874526f4b63e4349d
ayuki_j <ajjt66554@gmail.com>
2016-11-26 16:52:18 +0900

    commentモデル修正

--- myapp/app/models/comment.rb
+++ myapp/app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :movie
   belongs_to :user
-  validate :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 140 }
 end
