68cbd989590dc4b4f5191b4cadaec5ba536b8f62
ayuki_j <ajjt66554@gmail.com>
2016-11-26 16:55:28 +0900

    userモデル編集

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :movies
+  validates :name, presence:true
+  validates :area, presence:true
+  validates :school, presence:true
+  validates :schooltype, presence:true
+  validates :schoolgrade, presence:true
+  validates :subject, presence:true
+  validates :email, presence:true
 end
