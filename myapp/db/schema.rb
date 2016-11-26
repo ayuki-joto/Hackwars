3d5fa4eeb89bb9d8f9c4d78d37d8f0959d35e91e
ebiryu <ebiryu.zatta@gmail.com>
2016-11-26 16:09:00 +0900

    added association

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126050014) do
+ActiveRecord::Schema.define(version: 20161126065424) do
 
   create_table "comments", force: :cascade do |t|
     t.datetime "date"
@@ -19,6 +19,33 @@ ActiveRecord::Schema.define(version: 20161126050014) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "comments", [nil], name: "index_comments_on_user_id"
+
+  create_table "evaluations", force: :cascade do |t|
+    t.integer  "perspicuity"
+    t.integer  "device"
+    t.integer  "voice"
+    t.integer  "concern"
+    t.integer  "speed"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "evaluations", [nil], name: "index_evaluations_on_movie_id"
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "subject"
+    t.string   "schooltype"
+    t.string   "school"
+    t.integer  "schoolgrade"
+    t.text     "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "movies", [nil], name: "index_movies_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
