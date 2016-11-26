1f38276211c9a6408e9507c2bffd222097c5d24b
ayuki_j <ajjt66554@gmail.com>
2016-11-26 16:18:00 +0900

    usermodel編集

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  validate :name,presence: true
+  validate :area,presence: true
+  validate :schooltype, presence: true
+  validate :school, presence: true
+  validate :schoolgrade,presence: true
+  validate :subject,presence: true
+  validate :email,presence: true
+
 end
