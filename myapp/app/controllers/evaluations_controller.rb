b0c5c4183a281f60ec3430ae89023711065d6110
ebiryu <ebiryu.zatta@gmail.com>
2016-11-27 13:10:46 +0900

    add radar chart to evaluation/index

--- myapp/app/controllers/evaluations_controller.rb
+++ myapp/app/controllers/evaluations_controller.rb
@@ -5,6 +5,19 @@ class EvaluationsController < ApplicationController
   # GET /evaluations.json
   def index
     @evaluations = Evaluation.all
+    @ev_perspicuity = @ev_device = @ev_voice = @ev_concern = @ev_speed = 0
+    @evaluations.each do |evaluation|
+      @ev_perspicuity += evaluation.perspicuity.to_f / @evaluations.size
+      @ev_device += evaluation.device.to_f / @evaluations.size
+      @ev_voice += evaluation.voice.to_f / @evaluations.size
+      @ev_concern += evaluation.concern.to_f / @evaluations.size
+      @ev_speed += evaluation.speed.to_f / @evaluations.size
+    end
+    @ev_perspicuity = @ev_perspicuity.round(2)
+    @ev_device = @ev_device.round(2)
+    @ev_voice = @ev_voice.round(2)
+    @ev_concern = @ev_concern.round(2)
+    @ev_speed = @ev_speed.round(2)
   end
 
   # GET /evaluations/1
