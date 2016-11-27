282552d3ddb5220de84f37dbe320b46445a19cd9
ebiryu <ebiryu.zatta@gmail.com>
2016-11-27 14:24:55 +0900

    add validate to evaluation

--- myapp/app/models/evaluation.rb
+++ myapp/app/models/evaluation.rb
@@ -1,3 +1,8 @@
 class Evaluation < ActiveRecord::Base
   belongs_to :movie
+  validates :perspicuity, presence: true, numericality: {less_than: 6, greater_than: 0}
+  validates :device, presence: true, numericality: {less_than: 6, greater_than: 0}
+  validates :voice, presence: true, numericality: {less_than: 6, greater_than: 0}
+  validates :concern, presence: true, numericality: {less_than: 6, greater_than: 0}
+  validates :speed, presence: true, numericality: {less_than: 6, greater_than: 0}
 end
